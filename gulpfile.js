68860daa50581c919e669b94b32aee97ab88853b
charlesmartinreed <charlesmartinreed@icloud.com>
2019-05-27 18:14:40 -0500

    Refactor gulpfile and html.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 "use strict";
 
-var gulp = require("gulp");
-var browsersync = require("browser-sync").create();
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const browsersync = require("browser-sync").create();
 
 // BrowserSync task
 function browserSync(done) {
@@ -14,10 +15,26 @@ function browserSync(done) {
   done();
 }
 
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+function css() {
+  return gulp
+    .src("/css/*.scss")
+    .pipe(sass({ outputStyle: "expanded" }))
+    .pipe(gulp.dest("./css"))
+    .pipe(browsersync.stream());
+}
+
 function watchFiles() {
-  gulp.watch(["*.html", "./css/*.css"]).on("change", browsersync.reload);
+  gulp
+    .watch(["*.html", "./css/*.scss"])
+    .on("change", gulp.series(css, browserSyncReload));
 }
 
 const watch = gulp.parallel(watchFiles, browserSync);
 
+exports.css = css;
 exports.watch = watch;
